6d29e2370f1148233e46001746042c88b4e5d792
basonpark <bp2470@columbia.edu>
2025-03-20 19:12:59 +0900

    initial commit for defi stablecoin tutorial

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -29,13 +29,6 @@ import { ERC20Burnable, ERC20 } from "@openzeppelin/contracts/token/ERC20/extens
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
- * @title DecentralizedStableCoin
- * @author Patrick Collins
- * Collateral: Exogenous
- * Minting (Stability Mechanism): Decentralized (Algorithmic)
- * Value (Relative Stability): Anchored (Pegged to USD)
- * Collateral Type: Crypto
- *
 * This is the contract meant to be owned by DSCEngine. It is a ERC20 token that can be minted and burned by the
 DSCEngine smart contract.
  */
